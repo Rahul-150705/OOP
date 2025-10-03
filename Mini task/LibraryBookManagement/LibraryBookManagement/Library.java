3aea3229dc81364b9e3643fd874a00921a08e0e4
Rahul P <rahul.cd23@bitsathy.ac.in>
2025-10-03 19:35:05 +0530

    Add files via upload

---
+++ Mini task/LibraryBookManagement/LibraryBookManagement/Library.java
@@ -0,0 +1,14 @@
+package lib;
+
+public class Library {
+	int id;
+	String title;
+	int isAvailable;
+	public Library(int id, String title, int isAvailable) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.isAvailable = isAvailable;
+	}
+}
+
